17b4fcd6a4c8b7a5f1dcda8539223b2014fa211f
Lucas Medeiros <92602681+Qluks@users.noreply.github.com>
2023-11-25 21:52:15 -0300

    React Native Web

---
+++ App.web.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>React Native Web!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#C3E8BD',
+    paddingTop: 40,
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: '#ADBDFF',
+    padding: 5,
+    marginVertical: 20,
+    alignSelf: 'flex-start',
+  },
+  title: {
+    fontSize: 40,
+  },
+});
+
+export default App;
